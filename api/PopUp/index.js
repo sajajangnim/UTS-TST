85c1228bfc2ed004ebe6d2593c44c8a94818bd7a
sajajangnim <jinggamw@gmail.com>
2020-10-20 01:40:32 +0700

    aaa

--- api/PopUp/index.js
+++ api/PopUp/index.js
@@ -1,9 +1,33 @@
 module.exports = async function (context, req) {
 
     context.res = {
-        
-        body: {
-            text: "Hello there,\nfirst name: spring\nlastname: day"
+        const first = req.body.first 
+        const last = req.body.last
+        if (first && last) {
+            const payload = {
+              name: `${first} ${last}`
+            }
+      
+            await container.items.create(payload)
+            context.res = {
+              status: 200,
+              body: {
+                  name: payload.name
+              }
+            }
+          } else {
+            context.res = {
+              status: 400,
+              body: {
+                  message: "Required body not found"
+              }
+            }
+          }
+        } catch (err) {
+          context.res = {
+            status: 500,
+            body: err.message
+          }
         }
+      }
     };
-}
